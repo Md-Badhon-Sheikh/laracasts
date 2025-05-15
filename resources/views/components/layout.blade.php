ba9dc198f0baeac2805b920d51929a7a840ed052
Md-Badhon-Sheikh <badhon0854@gmail.com>
2025-05-15 16:29:56 +0600

    first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    <div class="navbar bg-gray-800 shadow-sm">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <img src="{{ asset('images/laravel.svg') }}" alt="Logo">
+                </div>
+                <ul
+                    tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <nav>
+                        <x-nav-link href="/">Home</x-nav-link>
+                        <x-nav-link href="/about">About</x-nav-link>
+                        <x-nav-link href="/contact">Contact</x-nav-link>
+                    </nav>
+
+                </ul>
+            </div>
+            <img src="{{ asset('images/laravel.svg.png') }}" width="40px" alt="Logo">
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+                <nav class="">
+                    <x-nav-link href="/" class="  px-1 text-lg font-semibold">Home</x-nav-link>
+                    <x-nav-link href="/about" class="px-1 text-lg font-semibold">About</x-nav-link>
+                    <x-nav-link href="/contact" class="px-1 text-lg font-semibold">Contact</x-nav-link>
+                </nav>
+
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <a class="btn">Button</a>
+        </div>
+    </div>
+    <h2 class="text-xl font-bold">{{$heading}}</h2>
+
+    
+    {{$slot}}
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
+    <h1 class="text-xl font-semibold mt-10">Hello This is Contact page!!!</h1>
+
+</x-layout>
